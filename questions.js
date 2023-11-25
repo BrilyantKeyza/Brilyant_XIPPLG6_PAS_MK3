d794bf3591cef31c0dc1a212c351f34e6305c52c
BrilyantKeyza <132529479+BrilyantKeyza@users.noreply.github.com>
2023-11-25 23:43:46 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Apa fungsi utama UI/UX designer?",
+        answer: "C. Merancang antarmuka pengguna",
+        options: [
+            "A. Memasang perangkat lunak",
+            "B. Menganalisis data keuangan",
+            "C. Merancang antarmuka pengguna",
+            "D. Mengembangkan algoritma kecerdasan buatan"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Apa peran prototyping dalam desain UI/UX?",
+        answer: "D. Membuat representasi visual interaktif",
+        options: [
+            "A. Menciptakan model bisnis",
+            "B. Menghasilkan kode program",
+            "C. Menyusun laporan keuangan",
+            "D. Membuat representasi visual interaktif"
+        ]
+    },
+    {
+        numb: 3,
+        question: " Mengapa wireframing penting dalam desain UI/UX?",
+        answer: "B. Representasi awal tanpa desain detail",
+        options: [
+            "A. Meningkatkan kecepatan internet",
+            "B. Representasi awal tanpa desain detail",
+            "C. Membuat desain interaktif",
+            "D.  Proses meningkatkan kecepatan responsif"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Apa perbedaan antara UI dan UX?",
+        answer: "A. Desain visual vs. Pengalaman pengguna",
+        options: [
+            "A. Desain visual vs. Pengalaman pengguna",
+            "B. Kode HTML vs. Analisis data",
+            "C. Kamera dan mikrofon",
+            "D. Warna dan font"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Apa yang dimaksud dengan UI dalam desain?",
+        answer: "D. User Interface",
+        options: [
+            "A. User Interaction",
+            "B. User Inspiration",
+            "C. User Involvement",
+            "D. User Interface"
+        ]
+    }
+]
